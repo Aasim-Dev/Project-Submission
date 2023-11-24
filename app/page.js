5df0232153b1c77e4c51db06ccba3ae53d463567
Siddhant <siddhantgiri97@gmail.com>
2023-11-24 16:24:06 +0530

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,37 @@
-import Image from 'next/image'
+"use client";
+import { Suspense } from 'react';
+import Loading from "./loading"
 import styles from './page.module.css'
+import CollegeList from '@/components/CollegeList'
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="d-flex flex-column">
+      <header className="py-5">
+        <div className="container px-5">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-xxl-6">
+              <div className="text-center my-5">
+                <h1 className="fw-bolder mb-3"> Elevate your final year project experience and leave a lasting mark on the world of engineering.</h1>
+                <p className="lead fw-normal text-muted mb-4">Let's start by getting your institution onboard.</p>
+                <Link className="btn btn-primary btn-lg" href={`/addCollege`}>Get Started</Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="container-fluid px-4 pb-5">
+        <h1 className="mt-4 text-center py-5 fw-bolder">Select Institution</h1>
+        <div className="row px-5">
+          <Suspense fallback={<Loading />}>
+            <CollegeList />
+          </Suspense>
+        </div>
+      </div >
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
